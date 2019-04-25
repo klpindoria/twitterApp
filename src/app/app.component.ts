b9f2a5443a1ec30b94d1aa6b1108416bc784390c
klpindoria <44475172+klpindoria@users.noreply.github.com>
2019-04-25 17:57:11 +0100

    SplashScreen hide removed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 
 import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 @Component({
@@ -11,21 +10,18 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 export class AppComponent {
   constructor(
     private platform: Platform,
-    private splashScreen: SplashScreen,
     private statusBar: StatusBar
   ) {
     this.initializeApp();
   }
 
   initializeApp() {
-    if(this.platform.is("cordova" || "android")){
+    if(this.platform.is("android")){
       this.platform.ready().then(() => {
         // stops status bar overlaying view
         this.statusBar.overlaysWebView(false);
         // set status bar to black
         this.statusBar.styleBlackOpaque();
-        // hides startup splashscreen
-        this.splashScreen.hide();
       });
     } else if (this.platform.is("ios")) {
       this.platform.ready().then(() => {
@@ -33,8 +29,6 @@ export class AppComponent {
         this.statusBar.overlaysWebView(false);
         // set status bar to black
         this.statusBar.styleDefault();
-        // hides startup splashscreen
-        this.splashScreen.hide();
       });
     } else {
       this.platform.ready().then(() => {
